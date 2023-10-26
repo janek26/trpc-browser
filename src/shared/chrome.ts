7959464672d3e42fc7c1741b7e2f3818ce88806d
Janek <janek.rahrt@me.com>
2023-10-26 15:59:59 +0200

    fix: new autoReconnect

--- src/shared/chrome.ts
+++ src/shared/chrome.ts
@@ -5,7 +5,7 @@ type AllowCleanUpFunction = void | (() => void);
 /**
  * Creates a port and recreates a new port if the old one disconnects
  *
- * @param chrome - chrome API
+ * @param createPort - function to create a port
  * @param onConnect - callback when connected
  */
 export async function autoConnect(
@@ -23,5 +23,22 @@ export async function autoConnect(
     cleanUp?.();
     console.log('Port disconnected, reconnecting...');
     void autoConnect(createPort, onConnect);
+  });
+}
+
+/**
+ * Reconnects a port if it disconnects
+ * @param port - port to reconnect
+ * @param createPort - function to create a port
+ * @param onConnect - callback when connected
+ */
+export function autoReconnect(
+  port: chrome.runtime.Port,
+  createPort: () => chrome.runtime.Port,
+  onReconnect: (port: chrome.runtime.Port) => AllowCleanUpFunction,
+) {
+  port.onDisconnect.addListener(() => {
+    console.log('Port disconnected, reconnecting...');
+    void autoConnect(createPort, onReconnect);
   });
 }
