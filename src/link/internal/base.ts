8a2fbf1d6ab146075d6adbb66a8d6c4fffe0943f
Vladut <vladut@argent.xyz>
2023-12-13 08:44:38 +0200

    Fix trpc errors deserialization

--- src/link/internal/base.ts
+++ src/link/internal/base.ts
@@ -31,7 +31,7 @@ export const createBaseLink = <TRouter extends AnyRouter>(
             if (id !== trpc.id) return;
 
             if ('error' in trpc) {
-              const error = runtime.transformer.deserialize(trpc.error);
+              const error = trpc.error;
               observer.error(TRPCClientError.from({ ...trpc, error }));
               return;
             }
