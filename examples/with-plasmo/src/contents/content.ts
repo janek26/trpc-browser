e0bfc3f336b380dc438d04733f24b1378e23bcad
diegodelrieue <diego@argent.xyz>
2024-04-22 15:54:51 +0200

    fix: pass correct chrome connct instance

--- examples/with-plasmo/src/contents/content.ts
+++ examples/with-plasmo/src/contents/content.ts
@@ -1,8 +1,11 @@
 import { relay } from 'trpc-browser/relay';
 import { autoConnect } from 'trpc-browser/shared/chrome';
 
-void autoConnect(chrome, (port) => {
-  return relay(window, port); // return so it has the cleanup function for disconnect
-}).then(() => {
+void autoConnect(
+  () => chrome.runtime.connect(),
+  (port) => {
+    return relay(window, port); // return so it has the cleanup function for disconnect
+  },
+).then(() => {
   return console.log('Content script loaded - bridge initialized');
 });
